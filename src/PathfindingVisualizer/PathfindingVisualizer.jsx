c69f6e6166d51b91a205999065a49dc594a3cee7
GenMode <mohamad191414@gmail.com>
2023-10-20 16:20:04 -0400

    modified:   src/PathfindingVisualizer/PathfindingVisualizer.jsx

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -46,20 +46,22 @@ export default class PathfindingVisualizer extends Component {
           return;
         }
         setTimeout(() => {
-          
+          const node = visitedNodesInOrder[i];
+          document.getElementById(`node-${node.row}-${node.col}`).className =
+            'node node-visited';
         }, 10 * i);
-    
-    
+      }
+    }
 
-
-
-
-
-
-
-
-
-
+animateShortestPath(nodesInShortestPathOrder) {
+  for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+    setTimeout(() => {
+      const node = nodesInShortestPathOrder[i];
+      document.getElementById(`node-${node.row}-${node.col}`).className =
+       'node node-shortest-path';
+    }, 50 * i);
+  }
+}
 
 
 
